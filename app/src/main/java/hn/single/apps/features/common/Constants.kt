aadf149a97c97d6adda871dee0cce4a673755e74
hanchan112 <hanchan112@gmail.com>
2023-03-01 00:50:40 +0700

    - PostFragment - Retrofit, Okhttp, ViewModel, LiveData..

---
+++ app/src/main/java/hn/single/apps/features/common/Constants.kt
@@ -0,0 +1,10 @@
+package hn.single.apps.features.common
+
+object Constants {
+
+    const val BASE_URL = "https://jsonplaceholder.typicode.com"
+    const val CALL_TIMEOUT: Long = 30_000
+    const val CONNECT_TIMEOUT: Long = 30_000
+    const val READ_TIMEOUT: Long = 30_000
+    const val WRITE_TIMEOUT: Long = 30_000
+}
